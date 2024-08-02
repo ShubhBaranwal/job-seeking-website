658dbfd027dbea2c15d2d424d3a3180e751443c9
ShubhBaranwal <134757662+ShubhBaranwal@users.noreply.github.com>
2024-08-02 11:41:21 +0530

    Update dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose  from "mongoose";
 
 export const dbConnection=()=>{
-    mongoose.connect(process.env.MONGODB_URL_LOCALHOST,{
+    mongoose.connect(process.env.MONGODB_URL,{
         dbName:"MERN_STACK_JOB_SEEKING"
     }).then(()=>{
         console.log("connected to database");
